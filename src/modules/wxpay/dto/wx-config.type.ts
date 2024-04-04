54e91183448c81681ca9f28305559b243ac2c170
806960230 <806960230@qq.com>
2024-04-04 21:51:23 +0800

    server

--- src/modules/wxpay/dto/wx-config.type.ts
+++ src/modules/wxpay/dto/wx-config.type.ts
@@ -1,11 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 
-// appId: 'wx2421b1c4370ec43b', // 公众号ID，由商户传入
-// timeStamp: new Date().getMilliseconds(), // 时间戳，自1970年以来的秒数
-// nonceStr: 'e61463f8efa94090b1f366cccfbbb444', // 随机串
-// package: 'prepay_id=u802345jgfjsdfgsdg888',
-// signType: 'MD5', // 微信签名方式：
-// paySign: '70EA570631E4BB79628FBCA90534C63FF7FADD89', // 微信签名
 @ObjectType()
 export class WxConfig {
   @Field({
--- src/modules/wxpay/wxpay.resolver.ts
+++ src/modules/wxpay/wxpay.resolver.ts
@@ -32,12 +32,7 @@ export class WxpayResolver {
     @Inject(WECHAT_PAY_MANAGER) private wxPay: WxPay,
   ) {}
 
-  // appId: 'wx2421b1c4370ec43b', // 公众号ID，由商户传入
-  // timeStamp: '1395712654', // 时间戳，自1970年以来的秒数
-  // nonceStr: 'e61463f8efa94090b1f366cccfbbb444', // 随机串
-  // package: 'prepay_id=up_wx21201855730335ac86f8c43d1889123400',
-  // signType: 'RSA', // 微信签名方式：
-  // paySign: 'oR9d8PuhnIc+YZ8cBHFCwfgpaK9gd7vaRvkYD7rthRAZ\/X+QBhcCYL21N7cHCTUxbQ+EAt6Uy+lwSN22f5YZvI45MLko8Pfso0jm46v5hqcVwrk6uddkGuT+Cdvu4WBqDzaDjnNa5UK3GfE1Wfl2gHxIIY5lLdUgWFts17D4WuolLLkiFZV+JSHMvH7eaLdT9N5GBovBwu5yYKUR7skR8Fu+LozcSqQixnlEZUfyE55feLOQTUYzLmR9pNtPbPsu6WVhbNHMS3Ss2+AehHvz+n64GDmXxbX++IOBvm2olHu3PsOUGRwhudhVf7UcGcunXt8cqNjKNqZLhLw4jq\/xDg==', // 微信签名
+  
   @Mutation(() => WxConfigResult)
   async getWxpayConfig(
     @CurUserId() userId,
